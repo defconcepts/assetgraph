b5c6db55fba8330fd572d1a520876ccd7f93b8cc
Gustav Nikolaj Olsen <gno@one.com>
2015-10-09 20:58:13 +0200

    fix relations/Html* (7 failing)

--- test/relations/HtmlKnockoutContainerless.js
+++ test/relations/HtmlKnockoutContainerless.js
@@ -14,10 +14,11 @@ describe('relations/HtmlKnockoutContainerless', function () {
                 expect(assetGraph, 'to contain relation', 'HtmlKnockoutContainerless');
 
                 var javaScript = assetGraph.findAssets({type: 'JavaScript', isInline: true})[0];
-                javaScript.parseTree.body[0].body.properties.push(new uglifyJs.AST_ObjectKeyVal({
-                    key: 'yup',
-                    value: new uglifyJs.AST_String({value: 'right'})
-                }));
+                javaScript.parseTree.body[0].expression.properties.push({
+                    type: 'Property',
+                    key: { type: 'Identifier', name: 'yup' },
+                    value: { type: 'Literal', value: 'yup', raw: '\'yup\''}
+                });
                 javaScript.markDirty();
 
                 expect(assetGraph.findAssets({type: 'Html'})[0].text, 'to match', /yup/);
